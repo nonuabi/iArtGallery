9d44c39ec1fe0d0035dc2e33eabd3a35803176cc
Abhishke Verma <nonuabi@gmail.com>
2021-11-22 16:39:24 +0530

    Add Cart

---
+++ app/models/line_item.rb
@@ -0,0 +1,9 @@
+class LineItem < ApplicationRecord
+    belongs_to :cart
+    belongs_to :store
+
+    def total_price
+        self.store.price * quantity
+    end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_171724) do
+ActiveRecord::Schema.define(version: 2021_11_22_052209) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2021_11_21_171724) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "cart_id"
+    t.integer "store_id"
+    t.integer "quantity", default: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "stores", force: :cascade do |t|
