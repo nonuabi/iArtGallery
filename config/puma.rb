3f500d26b59de0e93478e4772f90f8b8a3521654
Abhishek Verma <abhishek.verma@rootquotient.com>
2024-06-11 23:54:20 +0530

    chore: Changed puma port

--- config/puma.rb
+++ config/puma.rb
@@ -1,22 +1,22 @@
 # config/puma.rb
 
 # Specifies the number of threads per worker
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }
 threads threads_count, threads_count
 
 # Specifies the port to listen on (useful for debugging)
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch('PORT') { 3001 }
 
 # Specifies the environment that Puma will run in
-environment ENV.fetch("RAILS_ENV") { "production" }
+environment ENV.fetch('RAILS_ENV') { 'production' }
 
 # Specifies the PID file location
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 
 # Specifies the number of worker processes to boot in clustered mode
 # Workers are forked web server processes. If using threads and workers together,
 # the concurrency of the application will be `threads * workers`.
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch('WEB_CONCURRENCY') { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
