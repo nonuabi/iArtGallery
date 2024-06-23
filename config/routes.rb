49bcf00de11fc869d89378a2c31ad5305a47106c
Abhishek Verma <abhishek.verma@rootquotient.com>
2024-06-23 19:36:44 +0530

    Update styles for home page and navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  get 'home/index'
+
+  devise_for :users
+
   resources :line_items
   resources :carts
   resources :stores
-  devise_for :users
-  get 'home/index'
-  root 'home#index'
+
   get 'gallery/index'
   get 'gallery/checkout'
   post 'gallery/checkout'
   get 'gallery/purchase_complete'
   get 'gallery/search'
   post 'gallery/search'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
